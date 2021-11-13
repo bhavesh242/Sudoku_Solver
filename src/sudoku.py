6fc75ac7d29750d4a9f15addf154b7edd54411df
Bhavesh Agrawal <bhavesh.agrawal242@gmail.com>
2021-11-13 02:06:11 -0800

    .

--- src/sudoku.py
+++ src/sudoku.py
@@ -1,7 +1,8 @@
 
 ROWSIZE = 9
 COLSIZE = 9
-empty_boxes_num = 0
+TOTAL_CELLS = 81
+empty_cells_num = 0
 
 
 def main():
@@ -14,33 +15,67 @@ def main():
               [0, 0, 3, 0, 0, 7, 0, 0, 0],
               [0, 4, 1, 6, 5, 3, 0, 0, 2],
               [2, 0, 0, 0, 4, 0, 0, 6, 0]]
-    
+
     solve_puzzle_for_check(puzzle)
-    
+
 
 def solve_puzzle_for_check(puzzle):
-    get_initial_empty_boxes(puzzle)
+    get_initial_empty_cells(puzzle)
+    domains = get_all_domains(puzzle)
+    forward_checking_with_heuristics(puzzle)
 
 
-    if empty_boxes_num == ROWSIZE*COLSIZE:
+def forward_checking_with_heuristics(puzzle):
+
+    if empty_cells_num == TOTAL_CELLS:
         print("The Sudoku was Solved")
-        for row in range(len(puzzle)):
-            print(row)
-    else:
-        print(empty_boxes_num)
+        print_op(puzzle)
+        return
 
 
 # Optimization: We store the number of empty boxes initially and change it as we assign values, so that we do not have to recalculate them on every recurisve call
-def get_initial_empty_boxes(puzzle):
-    global empty_boxes_num
+def get_initial_empty_cells(puzzle):
+    global empty_cells_num
     for i in range(ROWSIZE):
         for j in range(COLSIZE):
             if puzzle[i][j] == 0:
-                empty_boxes_num = empty_boxes_num + 1
+                empty_cells_num = empty_cells_num + 1
+
+
+def get_all_domains(puzzle):
+    domains = []
+    for i in range(ROWSIZE):
+        for j in range(COLSIZE):
+            if puzzle[i][j] != 0:
+                domains.append(None)
+            else:
+                domains.append(get_cell_domain(i, j, puzzle))
+
+    return domains
+
+
+def get_cell_domain(row, col, puzzle):
+    exclude_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+    box_number = [row//3, col//3]
+    box_elements = get_box_elements(box_number, puzzle)
+    return exclude_set.intersection(box_elements)
+
+
+def get_box_elements(box_number, puzzle):
+    box_elements = set()
+    for row in range(box_number[0]*3, (box_number[0]+1)*3):
+        for col in range(box_number[1]*3, (box_number[1]+1)*3):
+            if(puzzle[row][col] == 0):
+                continue
+            else:
+                box_elements.add(puzzle[row][col])
+    return box_elements
+
+
+def print_op(puzzle):
+    for row in range(ROWSIZE):
+        print(row)
+
 
 if __name__ == "__main__":
     main()
-
-
-
-
